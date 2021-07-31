396ae113718a2e05f6f259f628fa31cbfe23a9fc
Chethana Kumar <chethanakumar@Chethanas-MacBook-Pro.local>
2021-07-31 11:25:07 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Getting Started with Create React App
+# Cookie consent wrapper app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a utility built on top of react-cookie-consent. This will allow creation of a customized cookie wrapper
+that can be used in webpage.
 
 ## Available Scripts
 
@@ -41,30 +42,4 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 
 ## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Create a minified java-script file that can be used for cookie-consent
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CookieConsent from 'cookie-consent-lib';
+import button from './index.css';
+
+
+const COOKIE_EXPIRY_TIME = 150
+const defaultMessage = 'This website is using cookies.';
+const defaultButtonStyle = button;
+
+
+function getCookieConsentInput(element){
+  const el = document.getElementById(element)
+  if(element === 'data-id-cookie-consent-bg-color' && el.getAttribute('data-param') === 'default') {
+    return ; 
+  } 
+  if (element === 'data-id-cookie-consent-button-style' && el.getAttribute('data-param') === 'default'){
+    return;
+  }
+
+  if(el){
+    return el.getAttribute('data-param');
+  }  
+  return;
+}
+
+const backgroundColor = getCookieConsentInput('data-id-cookie-consent-bg-color');
+const bgColor = backgroundColor || '#2B373B';
+
+// Create consent object here
+const consent = {}
+consent.expires = COOKIE_EXPIRY_TIME;
+consent.message = getCookieConsentInput('data-id-cookie-consent-message') || defaultMessage;
+consent.location = getCookieConsentInput('data-id-cookie-consent-location');
+consent.buttonText = getCookieConsentInput('data-id-cookie-consent-button-text');
+consent.style = {background: bgColor};
+consent.buttonStyle = getCookieConsentInput('data-id-cookie-consent-button-style') || defaultButtonStyle;
+consent.enableDeclineButton = getCookieConsentInput('data-id-cookie-enable-decline');
+consent.cookieName = 'consentCookie';
+
+function ConsentWrapper(props) {
+  const { message, ...restProps} = props.props;
+  return (
+    <CookieConsent {...restProps}>{message}</CookieConsent>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ConsentWrapper props = {consent}/>,
+  document.getElementById('light-beam-cookie-consent')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
